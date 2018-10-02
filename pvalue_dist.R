9c18003b458b955d2e275890f066c3e3bf0448ff
mingjingsi <30649118+mingjingsi@users.noreply.github.com>
2018-10-02 19:48:19 +0800

    Add files via upload

---
+++ pvalue_dist.R
@@ -0,0 +1,132 @@
+##### Simulations if P-values are not from beta distribution #####
+# Vary dist="near_normal", "skew", "big_normal" to get Figure S33 in Supplementary Document
+
+library(MASS)
+library(pbivnorm)
+library(mvtnorm)
+library(pROC)
+
+comp_FDR <- function(true, est){
+  
+  t <- table(true, est)
+  if (sum(est)==0){
+    FDR.fit <- 0
+  }
+  else if (sum(est)==length(est)){
+    FDR.fit <- t[1]/(t[1]+t[2])
+  }
+  else{
+    FDR.fit <- t[1,2]/(t[1,2]+t[2,2])
+  }
+  
+  return(FDR.fit)
+}
+
+spiky <- function(N){
+  r <- runif(N)
+  z <- rnorm(N, 0, 0.25)
+  if(sum(r <= 0.2) != 0)
+    z[which(r <= 0.2)] <- rnorm(sum(r <= 0.2), 0, 0.5)
+  if(sum(r > 0.2 & r <= 0.4) != 0)
+    z[which(r > 0.2 & r <= 0.4)] <- rnorm(sum(r > 0.2 & r <= 0.4), 0, 1)
+  if(sum(r > 0.8) != 0)
+    z[which(r > 0.8)] <- rnorm(sum(r > 0.8), 0, 2)
+  
+  return(z)
+}
+
+near_normal <- function(N){
+  r <- runif(N)
+  z <- rnorm(N, 0, 1)
+  if(sum(r <= 1/3) != 0)
+    z[which(r <= 1/3)] <- rnorm(sum(r <= 1/3), 0, 2)
+  
+  return(z)
+}
+
+skew <- function(N){
+  r <- runif(N)
+  z <- rnorm(N, -2, 2)
+  if(sum(r <= 0.25) != 0)
+    z[which(r <= 0.25)] <- rnorm(sum(r <= 0.25), -1, 1.5)
+  if(sum(r > 0.25 & r <= (0.25+1/3)) != 0)
+    z[which(r > 0.25 & r <= (0.25+1/3))] <- rnorm(sum(r > 0.25 & r <= (0.25+1/3)), 0, 1)
+  if(sum(r > 5/6) != 0)
+    z[which(r > 5/6)] <- rnorm(sum(r > 5/6), 1, 1)
+  
+  return(z)
+}
+
+big_normal <- function(N){
+  r <- runif(N)
+  z <- rnorm(N, 0, 4)
+  
+  return(z)
+}
+
+K <- 2                 # No. of traits
+M <- 100000            # No. of SNPs
+D <- 5                 # No. of annotations
+beta0 <- -1            # intercept of the probit model
+beta0 <- rep(beta0, K)
+set.seed(1)
+beta    <- matrix(rnorm(K*D), K, D)  # coefficients of annotations
+A.perc <- 0.2                        # the proportion the entries in X is 1
+A         <- rep(0, M*D)             # the design matrix of annotation
+indexA    <- sample(M*D, M*D*A.perc)
+A[indexA] <- 1
+A         <- matrix(A, M, D)
+r <- 1                               # the relative signal strengh between annotated part and un-annotated part
+sigmae2 <- var(A %*% t(beta))/r
+beta    <- beta/sqrt(diag(sigmae2))
+beta    <- cbind(as.matrix(beta0), beta)
+
+rho <- 0       # correlation between the two traits
+R <- matrix(c(1, rho, rho, 1), K, K)
+
+dist <- "spiky"
+
+rep <- 500  # repeat times
+
+library(LPM)
+
+# function to generate data
+generate_data <- function(M, K, D, A, beta, R, dist){
+  
+  Z <- cbind(rep(1, M), A) %*% t(beta) + mvrnorm(M, rep(0, K), R)
+  
+  indexeta <- (Z > 0)
+  eta      <- matrix(as.numeric(indexeta), M, K)
+  
+  Pvalue <- NULL
+  
+  fz <- match.fun(dist)
+  
+  for (k in 1:K){
+    z <- fz(sum(indexeta[, k]))
+    Pvalue_tmp <- runif(M)
+    Pvalue_tmp[indexeta[, k]] <- pnorm(abs(z), lower.tail = FALSE)*2
+    
+    Pvalue <- c(Pvalue, list(data.frame(SNP = seq(1, M), p = Pvalue_tmp)))
+    
+  }
+  
+  names(Pvalue) <- paste("P", seq(1, K), sep = "")
+  
+  A <- data.frame(SNP=seq(1,M), A)
+  
+  return( list(Pvalue = Pvalue, A = A, beta = beta, eta = eta))
+}
+
+pvalue_rho <- numeric(rep)
+
+for (l in 1:rep){
+  data <- generate_data(M, K, D, A, beta, R, dist)
+  Pvalue <- data$Pvalue
+  X      <- data$A
+
+  fit <- bLPM(Pvalue, X = X)
+  pvalue_rho[i] <- test_rho(fit)
+}
+
+result <- sum(pvalue_rho < 0.05)/rep
